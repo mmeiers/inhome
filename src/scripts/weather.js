4b0692356edc559ed0dcde1a5cd864a389fc45d1
Mathew Meiers <mlm@risedisplay.com>
2016-06-30 08:02:53 -0500

    weather.js

---
+++ src/scripts/weather.js
@@ -0,0 +1,21 @@
+    // v3.1.0
+//Docs at http://simpleweatherjs.com
+$(document).ready(function() {
+  $.simpleWeather({
+    location: 'Lawrence, KS',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      //html = '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html = '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+      html += '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
